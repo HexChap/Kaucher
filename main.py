0dd81017f2de7eb8d80d45bab8435c3040ac0ba3
unknown <TerraDragon.ua@gmail.com>
2022-07-08 02:39:39 +0300

    First menu

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello, world!")
+import os
+
+from pymenu import *
+from pymenu import colorpy
+
+
+def main():
+    columns = ["Сервер", "Версия"]
+    servers = {
+        "1.7.10": ["Tesla", "SkyFactory"]
+    }
+
+    server_select_menu = ColumnsMenu(
+        "Выберите сборку для запуска",
+        columns, exit_option=ExitOption("Выйти", lambda: [colorpy.cls(), os._exit(0)])
+    )
+
+    def callback(o: OptionRow):
+        # TODO: Start server
+
+        server_menu = Menu(
+            f"Сборка {o.server_name} была запущена!",
+            exit_option=ExitOption("Назад", lambda: server_select_menu.start())
+        )
+
+        server_menu.start()
+
+    for sv in servers.keys():
+        server_select_menu.add_rows([
+            OptionRow(columns, [sn, sv], callback, server_name=sn)
+            for sn in servers.get(sv)
+        ])
+
+    while True:
+        server_select_menu.start()
+
+
+if __name__ == "__main__":
+    main()
