05051ecadcbcd5a0d56cfdcab0c80ac1e1edf58d
TerraFaster <TerraDragon.ua@gmail.com>
2022-07-21 19:24:50 +0300

    Progress save

--- main.py
+++ main.py
@@ -1,87 +1,141 @@
+from dataclasses import dataclass
 import os
-import psutil
-import time
-
-from pymenu import *
-from pymenu import colorpy
 from threading import Thread
+from tkinter import Button, Canvas, Tk
+import tkinter
+
+from app.controllers import launcher, extractor
 
 
-def show_mp_select_menu(prev_menu: Menu):
-    columns = ["Сервер", "Версия"]
-    modpacks = {
-        "1.7.10": ["Tesla", "SkyFactory"]
-    }
+class Utils:
+    @staticmethod
+    def screen_size(tk_: Tk) -> list[int]:
+        """
+        Returns the width and height of the user's screen in pixels.
 
-    menu = ColumnsMenu(
-        "Выберите сборку для запуска",
-        columns, exit_option=ExitOption("Назад", lambda: prev_menu.start())
-    )
+        :return: list[int] Screen width and height as list
+        """
+        return [tk_.winfo_screenwidth(), tk_.winfo_screenheight()]
 
-    def callback(o: OptionRow):
-        # TODO: Start modpack
 
-        modpack_menu = Menu(
-            f"Сборка {o.modpack_name} была запущена!",
-            exit_option=ExitOption("Назад", lambda: menu.start())
+@dataclass
+class Theme:
+    bg: str
+    outline: str
+    mp_bg: str
+    fg: str
+    font_family: tuple
+
+
+class App:
+    def __init__(self) -> None:
+        self._window_title = "Kaucher"
+        self._window_size = [650, 450]
+
+        self.theme = Theme(
+            bg="#161620",
+            outline="#1f2029",
+            mp_bg="#1a1b26",
+            fg="#e0e2e4",
+            font_family=("Arial", 11, "bold")
         )
 
-        modpack_menu.start()
+        self._root = Tk(self._window_title, self._window_title)
 
-    for sv in modpacks.keys():
-        menu.add_rows([
-            OptionRow(columns, [sn, sv], callback, modpack_name=sn)
-            for sn in modpacks.get(sv)
-        ])
+        self.init_window()
 
-    menu.start()
+        self._canv = Canvas(
+            self._root, bg=self.theme.bg, width=self._window_size[0], 
+            height=self._window_size[1], highlightthickness=0
+        )
+        self._canv.pack()
 
+    def init_window(self):
+        self._root.title(self._window_title)
 
-def show_account_update_menu(prev_menu: Menu):
-    menu = Menu(
-        "Запустите любую сборку из официального лаунчера Kaboom 2.0",
-        exit_option=ExitOption("Назад", lambda: prev_menu.start())
-    )
+        ss = Utils.screen_size(self._root)
+        self._root.geometry(
+            f"{self._window_size[0]}x{self._window_size[1]}"
+            f"+{ss[0] // 2 - self._window_size[0] // 2}"
+            f"+{ss[1] // 2 - self._window_size[1] // 2}"
+        )
 
-    def update_account():
-        while menu.is_active:
-            for proc in psutil.process_iter():
-                if proc.name() == "javaw.exe":
-                    cmd_line = proc.cmdline()
+        self._root.resizable(False, False)
 
-                    # if "accessToken" not in cmd_line:
-                    #     continue
+    def clear_screen(self):
+        self._canv.delete("all")
 
-                    proc.kill()
+    # Menus
+    def show_mp_startup_page(self, page: int = 1):
+        """
+        Displays all modpacks.        
+        
+        :param page:int=1: Determine which page of modpacks to display
+        """
+        self.clear_screen()
 
-                    menu.title = "Выберите аккаунт"
-                    menu.add_option(Option(
-                        cmd_line[cmd_line.index("--username")+1]
-                    ))
+        MP_FOR_PAGE = 6
+        mp_list = []
 
-                    return
+        for k, v in launcher.modpacks.items():
+            mp_list.extend([[mp, k] for mp in v])
 
-            time.sleep(0.5)
+        # Add buttons with mp names
+        for i, mp in enumerate(mp_list[(page-1)*MP_FOR_PAGE:page*MP_FOR_PAGE]):
+            self._canv.create_rectangle(
+                75 + ((i - 3 * (i > 2)) * 187.5), 60 + 190 * (i > 2),
+                75 + ((i - 3 * (i > 2)) * 187.5) + 125, 200 + 190 * (i > 2),
+                fill=self.theme.mp_bg, outline=self.theme.outline, width=1.5
+            )
 
-    Thread(target=update_account).start()
+            # Create modpack info text
+            self._canv.create_text(
+                137.5 + ((i - 3 * (i > 2)) * 187.5), 80 + 190 * (i > 2),
+                text=mp[0].title(), font=self.theme.font_family,
+                fill=self.theme.fg, justify="center"
+            )
+            self._canv.create_text(
+                137.5 + ((i - 3 * (i > 2)) * 187.5), 100 + 190 * (i > 2),
+                text=mp[1], font=self.theme.font_family,
+                fill=self.theme.fg, justify="center"
+            )
 
-    menu.start()
+            # Create Start mp button
+            # TODO: Callback (launch mp)
+            start_btn = Button(
+                self._canv, text="Запустить", bg=self.theme.bg, fg=self.theme.fg,
+                borderwidth=0, height=1, width=10, state="normal",
+            )
 
+            # Create Open mp folder button
+            # TODO: Callback (open mp folder)
+            x, y = 137.5 + 187.5 * ((i - 3 * (i > 2))), 140 + 190 * (i > 2)
+            self._canv.create_rectangle(
+                x-17, y-15, x+17, y+15, fill=self.theme.bg, outline=self.theme.outline, width=1.5
+            )
+            self._canv.create_text(
+                x, y-2, text="📁", fill=self.theme.fg, font=("Arial", 15)
+            )
 
-def main():
-    main_menu = Menu(
-        "Выберите действие",
-        exit_option=ExitOption("Выйти", lambda: [colorpy.cls(), os.exit(0)])
-    )
+            # open_folder_btn = Button(
+            #     self._canv, text="📂", bg=self.theme.bg, fg=self.theme.fg,
+            #     borderwidth=0, width=3, height=1, font="Arial 13"
+            #     # command=lambda mp=mp: os.startfile(
+            #     #     f"{ROOT_DIR}/data/minecraft/{mp[3]}/modpacks/{mp[0]}"
+            #     # )
+            # )
 
-    main_menu.add_options([
-        Option("Запустить сборку", lambda: show_mp_select_menu(main_menu)),
-        Option("Обновить данные", lambda: show_account_update_menu(main_menu))
-    ])
+            # Place buttons
+            self._canv.create_window(
+                137.5 + 187.5 * ((i - 3 * (i > 2))),
+                185 + 190 * (i > 2), window=start_btn
+            )
 
-    while True:
-        main_menu.start()
+    def run(self):
+        self._root.after(1000, self.show_mp_startup_page)
+        self._root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.run()
